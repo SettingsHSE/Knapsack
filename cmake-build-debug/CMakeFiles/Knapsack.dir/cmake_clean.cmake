60dcea9686533872aaec7c483afffe7671a85929
Софья Андреева <skandreeva@edu.hse.ru>
2020-04-22 17:06:54 +0300

    fix

---
+++ cmake-build-debug/CMakeFiles/Knapsack.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Knapsack.dir/main.cpp.obj"
+  "Knapsack.pdb"
+  "Knapsack.exe"
+  "Knapsack.exe.manifest"
+  "libKnapsack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Knapsack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
